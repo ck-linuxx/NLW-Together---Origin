5f8cf6fd0e73906fc39bc8727f2268cba8e0f254
Clark <amorimclark@gmail.com>
2021-07-29 12:29:11 -0300

    master

---
+++ main.js
@@ -0,0 +1,32 @@
+/*Abrir e fechar o menu*/
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for(const element of toggle){
+    element.addEventListener('click', function() {
+        nav.classList.toggle('show')
+    })
+}
+
+/*Quando clicar em um item do menu, esconder o menu*/
+const links = document.querySelectorAll('nav ul li a')
+
+for(const link of links){
+    link.addEventListener('click', function(){
+        nav.classList.remove('show')
+    })
+}
+
+/*Mudar oheader da pág quando der scroll*/
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function(){
+    if(this.window.scrollY >= navHeight) {
+        //maior que a altura do header
+        header.classList.add('scroll')
+    }else{
+        //menor que a altura do header
+        header.classList.remove('scroll')
+    }
+})
