b2f59797c90b7a5eb41fc345f01cf9ab482d900a
ck-linuxx <amorimclark@gmail.com>
2021-08-07 18:26:44 -0300

    master

--- main.js
+++ main.js
@@ -40,7 +40,10 @@ const swiper = new Swiper('.swiper-container', {
     mousewheel: true,
     keyboard: true,
     breakpoints: {
-        
+        767:{
+            slidesPerView: 2,
+            setWrapperSize: true
+        }
     }
 });
 
